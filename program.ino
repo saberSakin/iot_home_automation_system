12ad344916f0e905d08dd209adbad92dcfc40702
Mohammad Shakin <md.shakin18@gmail.com>
2024-03-15 19:27:10 +0600

    updates

--- program.ino
+++ program.ino
@@ -1,46 +1,53 @@
+#define BLYNK_USE_DIRECT_CONNECT
 #include <SoftwareSerial.h>
+SoftwareSerial BT(0, 1); // RX, TX
 
-SoftwareSerial bluetooth(0, 1); // RX, TX
+#define BLYNK_PRINT BT
+#include <BlynkSimpleSerialBLE.h>
 
-#define RELAY_1_PIN 2 // Pin for controlling first relay
-#define RELAY_2_PIN 3 // Pin for controlling second relay
-#define RELAY_3_PIN 4 // Pin for controlling third relay
+const int IN1 = 6;// Input for channel 1(Light)
+const int IN2 = 5;// Input for channel 2(Fan)
+int light,fan;
 
-char command; // Variable to store incoming command
+// You should get Auth Token in the Blynk App
+char auth[] = "YourAuthToken"; //Place the authentication token you got from the e-mail.
 
-void setup() {
-  pinMode(RELAY_1_PIN, OUTPUT);
-  pinMode(RELAY_2_PIN, OUTPUT);
-  pinMode(RELAY_3_PIN, OUTPUT);
+void setup()
+{
+  BT.begin(38400); //For HC-05 default speed is 38400
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  
+  // Blynk will work through Serial
+  // Do not read or write this serial manually in your sketch
+  Serial.begin(38400);
+  Blynk.begin(Serial, auth);
 
-  digitalWrite(RELAY_1_PIN, LOW); // Initially turn off relay 1
-  digitalWrite(RELAY_2_PIN, LOW); // Initially turn off relay 2
-  digitalWrite(RELAY_3_PIN, LOW); // Initially turn off relay 3
-
-  bluetooth.begin(9600); // Default baud rate for most Bluetooth modules
-  Serial.begin(9600); // For debugging purpose
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);    
 }
 
-void loop() {
-  if (bluetooth.available() > 0) { // If data is available to read
-    command = bluetooth.read(); // Read the incoming command from Bluetooth
-    executeCommand(command); // Execute the received command
+BLYNK_WRITE(V0){
+  light = param.asInt();
+}
+BLYNK_WRITE(V1){
+  fan = param.asInt();
+}
+
+void loop()
+{
+  Blynk.run();
+  //Control relay channel 1 based on value of variable 'Light'
+  if(light == 1){
+    digitalWrite(IN1, HIGH);  
+  }else{
+    digitalWrite(IN1, LOW);  
+  }
+
+  //Control relay channel 2 based on value of variable 'Fan'
+  if(fan == 1){
+    digitalWrite(IN2, HIGH);  
+  }else{
+    digitalWrite(IN2, LOW);  
   }
 }
-
-void executeCommand(char cmd) {
-  switch (cmd) {
-    case '1':
-      digitalWrite(RELAY_1_PIN, !digitalRead(RELAY_1_PIN)); // Toggle relay 1
-      break;
-    case '2':
-      digitalWrite(RELAY_2_PIN, !digitalRead(RELAY_2_PIN)); // Toggle relay 2
-      break;
-    case '3':
-      digitalWrite(RELAY_3_PIN, !digitalRead(RELAY_3_PIN)); // Toggle relay 3
-      break;
-    default:
-      break;
-  }
-}
-
